c38204c82014a8bf2be62da4751a5af01fc0aa98
neulii <neulii@gmx.at>
2022-07-14 19:38:06 +0200

    added deltaTime to update method

--- src/main.cpp
+++ src/main.cpp
@@ -16,14 +16,18 @@ int main()
     Field* f = new Field(10,10);
     entitys.push_back(f);
 
+    sf::Clock clock;
 
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
+
+               
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
+
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
@@ -36,9 +40,12 @@ int main()
 				window.close();
 			}
         }
-        long dT = 1;
 
-        update(dT);
+        //long dT = 1;
+
+        sf::Time time = clock.getElapsedTime(); 
+        clock.restart(); 
+        update(time.asMicroseconds());
         render(window);
         
     }
@@ -61,6 +68,6 @@ void render(sf::RenderWindow& window)
 void update(long dT)
 {
 
-
+    std::cout << dT << std::endl;
 
 }
